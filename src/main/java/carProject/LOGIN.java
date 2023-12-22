0e3cf92d921e80672e7d55709f38a5b690b9f7fe
samahsheeha1 <145497102+samahsheeha1@users.noreply.github.com>
2023-12-22 17:43:51 +0200

    Update LOGIN.java

--- src/main/java/carProject/LOGIN.java
+++ src/main/java/carProject/LOGIN.java
@@ -1,89 +1,69 @@
 package carProject;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-
-
 public class LOGIN {
-	    private String enteredEmail;
-	    private String enteredPassword;
-	    private static String tyu = "";
-		
+    private static final Logger LOGGER = Logger.getLogger(LOGIN.class.getName());
+    private String enteredEmail;
+    private String enteredPassword;
+    private static String tyu = "";
 
-	public boolean run(int p) {
+    public boolean run(int p) {
         return p == 1;
     }
 
-	public void navigateToLoginPage(String email, String password,List<User> userlist) {
-		   enteredPassword=password;
-      	  enteredEmail=email;
-      	  
-      		 
-      	    if (performLogin(enteredEmail, enteredPassword,userlist)) {
-      	    	System.out.println("Login successful.");
-      	    	System.out.println("\n");
-      	    	System.out.println("\n");
-      	    }
-      	    else {
-      	    	System.out.println("Invalid email or password. Please try again.");
-      	    
-      	    }
-      	  
-         
-	}
+    public void navigateToLoginPage(String email, String password, List<User> userlist) {
+        enteredPassword = password;
+        enteredEmail = email;
 
-	public boolean performLogin(String email, String password,List<User> userlist) {
-		
-		  for (User userr : userlist) {
-	            if (userr.getEmail().equals(email) && userr.getPassword().equals(password)  ) {
-	                tyu = userr.gettype();
-	                return true;
-	               
-	            }
-	        }
-	        return false;
-	}
+        if (performLogin(enteredEmail, enteredPassword, userlist)) {
+            LOGGER.info("Login successful.\n\n");
+        } else {
+            LOGGER.info("Invalid email or password. Please try again.");
+        }
+    }
 
-	  public boolean getA() {
-	        return tyu.equals("Admin");
-	    }
+    public boolean performLogin(String email, String password, List<User> userlist) {
+        for (User user : userlist) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                tyu = user.gettype();
+                return true;
+            }
+        }
+        return false;
+    }
 
-	    
-	    public boolean geti() {
-	        return tyu.equals("installer");
-	    }
+    public boolean getA() {
+        return tyu.equals("Admin");
+    }
 
-	    
-	    public boolean getc() {
-	        return tyu.equals("customer");
-	    }
+    public boolean geti() {
+        return tyu.equals("installer");
+    }
 
-		public boolean checkIfRegister(String email,List<User> userlist) {
-			for (User userr : userlist) {
-	            if (userr.getEmail().equals(email)) {
-	                return true;
-	            }
-	        }
-			return false;
-			
-			
-		}
-		
+    public boolean getc() {
+        return tyu.equals("customer");
+    }
 
-		public boolean ifvalid(String string) {
-			if(string.equals("") || !string.contains("@gmail.com") ){
-				System.out.println("invalid  please try again...");
-				return false;
-			}
-			return true;
-			
-		}
+    public boolean checkIfRegister(String email, List<User> userlist) {
+        for (User user : userlist) {
+            if (user.getEmail().equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	}
-
+    public boolean ifvalid(String string) {
+        if (string.equals("") || !string.contains("@gmail.com")) {
+            LOGGER.info("invalid email, please try again...");
+            return false;
+        }
+        return true;
+    }
+}
 
 
 
